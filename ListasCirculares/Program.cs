16ab8f32657a77ddb1e5f8e7537158a5425189d5
Juan Martinez <jfmg.64@gmail.com>
2023-02-28 14:39:51 -0700

    Se agregaron las pruebas de ListasSimplementeLigadas a listas Circulares (Previo a modificacion)

--- ListasCirculares/Program.cs
+++ ListasCirculares/Program.cs
@@ -10,7 +10,46 @@ namespace ListasCirculares
     {
         static void Main(string[] args)
         {
-            Lista circular = new Lista();
+            Lista lista = new Lista();
+
+            //Console.WriteLine(lista.RecorrerLista());
+
+            lista.Agregar("Uno");
+            lista.Agregar("Dos");
+            lista.Agregar("Tres");
+            //Console.WriteLine(lista.RecorrerLista());
+
+
+            lista.AgregarNodoInicio("Cero");
+            Console.WriteLine(lista.Recorrer());
+
+
+            Nodo NodoBusqueda = lista.Buscar("Uno");
+            if (NodoBusqueda != null)
+            {
+                Console.WriteLine(NodoBusqueda.Valor);
+            }
+            else
+            {
+                Console.WriteLine("No encontrado!");
+            }
+
+            Nodo NodoBusquedaAnterior = lista.BuscarAnterior("Dos");
+            if (NodoBusquedaAnterior != null)
+            {
+                Console.WriteLine(NodoBusquedaAnterior.Valor);
+            }
+            else
+            {
+                Console.WriteLine("No encontrado!");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Eliminando el uno");
+            lista.BorrarNodo("Uno");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.ReadKey();
         }
     }
 }
