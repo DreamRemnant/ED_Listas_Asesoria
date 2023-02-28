051e9269a71be50340433ed6aa6cf371c046eaa0
DreamRemnant <jfmg.64@gmail.com>
2023-02-27 22:54:13 -0700

    Se agregó Nodo desde ListasDoblementeLigadas. Se modificó las validaciones null a nodoInicial. Se modifico EliminarNodo y Agregar.

---
+++ ListasDoblementeLigadasCirculares/Lista.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadasCirculares
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo(anterior: nodoInicial,siguiente: nodoInicial);
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == nodoInicial)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = nodoInicial;
+            nodoInicial.Anterior = nodoInicial;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != nodoInicial)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void AgregarFinal(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != nodoInicial)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor,nodoActual, nodoInicial);
+            nodoActual.Siguiente = nodoNuevo;
+            nodoInicial.Anterior = nodoNuevo;
+        }
+
+        public void AgregarInicio(string valor)
+        {
+            if (ValidaVacio())
+            {
+                AgregarFinal(valor);
+                return;
+            }
+            Nodo nodoNuevo = new Nodo(valor,nodoInicial,nodoInicial.Siguiente);
+            nodoInicial.Siguiente.Anterior = nodoNuevo;
+            nodoInicial.Siguiente = nodoNuevo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio())
+            {
+                return null;
+            }
+
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != nodoInicial)
+            {
+                nodoActual = nodoActual.Siguiente;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+            }
+            return null;
+        }
+
+        public void EliminarNodo(string valor)
+        {
+            nodoActual = Buscar(valor);
+            if (nodoActual != null)
+            {
+                nodoActual.Anterior.Siguiente = nodoActual.Siguiente;
+                nodoActual.Siguiente.Anterior = nodoActual.Anterior;
+                nodoActual.Siguiente = null;
+                nodoActual.Anterior = null;
+            }
+        }
+    }
+}
---
+++ ListasDoblementeLigadasCirculares/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadasCirculares
+{
+    internal class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo Anterior { get; set; }
+        public Nodo Siguiente { get; set; }
+
+        public Nodo(string valor = "", Nodo anterior = null, Nodo siguiente = null)
+        {
+            Valor = valor;
+            Anterior = anterior;
+            Siguiente = siguiente;
+        }
+    }
+}
