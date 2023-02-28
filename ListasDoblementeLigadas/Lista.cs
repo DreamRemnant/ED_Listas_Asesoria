fc4c1ac7fc5f1004be2e98389fde9b09f55ec3ca
DreamRemnant <jfmg.64@gmail.com>
2023-02-27 21:37:22 -0700

    Se creó clase Nodo. Se creó Lista (basado en ListaSimplementeLigadas).

---
+++ ListasDoblementeLigadas/Lista.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadas
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void Agregar(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Siguiente = nodoNuevo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio())
+            {
+                return null;
+            }
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+                return null;
+            }
+
+        }
+    }
+}
